779b96d2d11b342ca152b72483bde3303ce5937d
HeleneMartin <helene.martin@fundingcircle.com>
2014-04-29 15:08:44 +0100

    correct migration field errors, create post folder where the new_post form should be

--- db/migrate/20140429114243_create_posts.rb
+++ db/migrate/20140429114243_create_posts.rb
@@ -2,11 +2,12 @@ class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
       t.string :title
-      t.text_field :description
-      t.text_field :url
-      t.text_field :img
-      
+      t.text :description
+      t.text :url
+    
+
       t.timestamps
+    end
   
   end
 end
