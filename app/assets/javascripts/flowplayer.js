d6778d7362bc1acaa5ae1ab10a94ead6fc1be8cc
HeleneMartin <helene.martin@fundingcircle.com>
2014-05-16 16:10:40 +0100

    fix flowplayer functionality

---
+++ app/assets/javascripts/flowplayer.js
@@ -0,0 +1,17 @@
+// $(function () { // ensure DOM is ready
+ 
+//    // this will install flowplayer into an element with id="player"
+//    $("#player").flowplayer({
+//       // one video: a one-member playlist
+//       playlist: [
+//          // a list of type-url mappings in picking order
+//          [
+//             { webm: "http://mydomain.org/video.webm" },
+//             { mp4:  "http://mydomain.org/video.mp4" },
+//             { ogg:  "http://mydomain.org/video.ogv" }
+//          ]
+//       ],
+//       ratio: 3/4 // video with 4:3 aspect ratio
+//    });
+ 
+// });
