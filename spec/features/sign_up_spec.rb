720bab194ba345b37bcd5907bc8e0d9f591ef0e2
helenemartin <helenemartin13@hotmail.com>
2014-05-19 17:29:19 +0100

    add international text

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -1,19 +1,18 @@
 require 'spec_helper'
 
 feature "Sign up" do
-  scenario "when signing up" do
+
+  scenario "when user Sign up" do
     
     visit '/'
     click_link 'Sign up'
-    fill_in :user_email, with: user.email
-    fill_in :user_password, with: user.password
-    fill_in :user_password_confirmation, with: user.password
+    fill_in :user_email, with: 'cat@miaw.cat'
+    fill_in :user_password, with: 'miaoumiaou'
+    fill_in :user_password_confirmation, with: 'miaoumiaou'
     click_button 'Sign up'
 
-    visit '/'
-
     page.should have_content "Logged in as cat@miaw.cat"
-    page.should have_content "Welcome! you have signed up successfully"
+    page.should have_content "Welcome! You have signed up successfully"
 
   end
 
