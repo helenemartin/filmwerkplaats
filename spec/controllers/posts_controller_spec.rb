95b724b17b3357d7fccdd9ff7d450285149f9e68
helenemartin <helenemartin13@hotmail.com>
2014-05-20 12:32:20 +0100

    create factory for posts, make test for post controller

---
+++ spec/controllers/posts_controller_spec.rb
@@ -0,0 +1,44 @@
+require 'spec_helper'
+
+describe PostsController do
+
+  let!(:post) {FactoryGirl.create(:post)}
+  let(:user) {FactoryGirl.create(:user)}
+  describe "Get 'post index'" do
+    it "should be succesful" do
+      get :index
+      response.should be_success
+    end
+  end
+
+  describe "Get 'post new'" do
+    it "should be successful" do
+      sign_in user
+      get :new
+      response.should be_success
+    end
+  end
+
+  describe "Get 'post edit'" do
+    it "should be successful" do
+      sign_in user
+      get :edit, {:id => post.id}
+      response.should be_success
+    end
+  end
+
+  describe "Delete 'destroy post'" do
+    before do
+      sign_in user
+    end
+    it "should redirect" do
+      delete :destroy, {:id => post.id}
+      response.should be_redirect
+    end
+    it "should delete" do
+      expect {
+        delete :destroy, {:id => post.id}
+      }.to change{Post.count}.from(1).to(0)
+    end
+  end
+end
---
+++ spec/factories/posts.rb
@@ -0,0 +1,8 @@
+
+FactoryGirl.define do
+  factory :post do
+    title "Todo"
+    description "a list of things"
+
+  end
+end
